047326e67003c33f727eac88d0f55456dee66c74
Md Alamgir Hossain Mojumder <hossainmojumdermdalamgir@gmail.com>
2026-05-02 09:39:11 +0600

    Add files via upload

--- global/sidebar.php
+++ global/sidebar.php
@@ -1,28 +1,27 @@
 <?php
 /**
- * Sidebar
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/global/sidebar.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     1.6.4
- * 
  * @cmsmasters_package 	Medical Clinic
- * @cmsmasters_version 	1.0.8
+ * @cmsmasters_version 	1.1.4
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+list($cmsmasters_layout) = medical_clinic_theme_page_layout_scheme();
+
+
+if ($cmsmasters_layout == 'r_sidebar') {
+	echo "\n" . '<!-- Start Sidebar -->' . "\n" . 
+	'<div class="sidebar">' . "\n";
+	
+	get_sidebar();
+	
+	echo "\n" . '</div>' . "\n" . 
+	'<!-- Finish Sidebar -->' . "\n";
+} elseif ($cmsmasters_layout == 'l_sidebar') {
+	echo "\n" . '<!-- Start Sidebar -->' . "\n" . 
+	'<div class="sidebar fl">' . "\n";
+	
+	get_sidebar();
+	
+	echo "\n" . '</div>' . "\n" . 
+	'<!-- Finish Sidebar -->' . "\n";
 }
-
-
-include(get_template_directory() . '/woocommerce/cmsmasters-framework/theme-style' . CMSMASTERS_THEME_STYLE . '/templates/global/sidebar.php');
